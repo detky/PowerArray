5b044e9467200fd5a42ca3885b086c631ec932b6
Sebastian <sebastian.menendez@gmail.com>
2015-08-30 13:04:18 +0200

    Update PowerArrayFunctionsDescriptor.js

--- PowerArrayFunctionsDescriptor.js
+++ PowerArrayFunctionsDescriptor.js
@@ -1,30 +1,58 @@
 window.pa.auxiliaryFunctionsDescriptor = [
-    {
+     {
+         Name: 'Between',
+         Targets: [
+             {
+                 TargetTypes: [
+                     window.pa.utils.DataTypes.ArrayOfPrimitives,
+                     window.pa.utils.DataTypes.String,
+                     window.pa.utils.DataTypes.Number,
+                     window.pa.utils.DataTypes.Date
+                 ],
+                 Overloads: [
+                     {
+                         Example: '.Between(from, to)',
+                         Parameters: [
+                             {
+                                 Name: 'from',
+                                 Type: 'any'
+                             },
+                             {
+                                 Name: 'to',
+                                 Type: 'any'
+                             }
+                         ]
+                     }
+                 ]
+             }
+         ]
+     }, {
         Name: 'In',
         Targets: [
             {
                 TargetTypes: [
-                            window.pa.utils.DataTypes.ArrayOfPrimitives,
-                            window.pa.utils.DataTypes.String,
-                            window.pa.utils.DataTypes.Number,
-                            window.pa.utils.DataTypes.Date
+                    window.pa.utils.DataTypes.ArrayOfPrimitives,
+                    window.pa.utils.DataTypes.String,
+                    window.pa.utils.DataTypes.Number,
+                    window.pa.utils.DataTypes.Date
                 ],
                 Overloads: [
                     {
-                        Parameters : [
+                        Example: '.In(list)',
+                        Parameters: [
                             {
                                 Name: 'list',
-                                Type: window.pa.utils.DataTypes.ArrayOfPrimitives,
-                                Example: '.In(list)'
+                                Type: window.pa.utils.DataTypes.ArrayOfPrimitives
                             }
                         ]
                     },
                     {
-                        Parameters : [
+                        Example: '.In(value0, value1, value2, value3, etc.)',
+                        Parameters: [
                             {
                                 Name: 'value',
                                 Type: 'any',
-                                Example: '.In(value0, value1, value2, value3, etc.)'
+                                Infinite: true
                             }
                         ]
                     }
@@ -35,55 +63,56 @@ window.pa.auxiliaryFunctionsDescriptor = [
     {
         Name: 'NotIn',
         Targets: [
-             {
-                 TargetTypes: [
-                             window.pa.utils.DataTypes.ArrayOfPrimitives,
-                             window.pa.utils.DataTypes.String,
-                             window.pa.utils.DataTypes.Number,
-                             window.pa.utils.DataTypes.Date
-                 ],
-                 Overloads: [
-                     {
-                         Parameters: [
-                             {
-                                 Name: 'list',
-                                 Type: window.pa.utils.DataTypes.ArrayOfPrimitives,
-                                 Example: '.NotIn(list)'
-                             }
-                         ]
-                     },
-                     {
-                         Parameters: [
-                             {
-                                 Name: 'value',
-                                 Type: 'any',
-                                 Example: '.NotIn(value0, value1, value2, value3, etc.)'
-                             }
-                         ]
-                     }
-                 ]
-             }
+            {
+                TargetTypes: [
+                    window.pa.utils.DataTypes.ArrayOfPrimitives,
+                    window.pa.utils.DataTypes.String,
+                    window.pa.utils.DataTypes.Number,
+                    window.pa.utils.DataTypes.Date
+                ],
+                Overloads: [
+                    {
+                        Example: '.NotIn(list)',
+                        Parameters: [
+                            {
+                                Name: 'list',
+                                Type: window.pa.utils.DataTypes.ArrayOfPrimitives
+                            }
+                        ]
+                    },
+                    {
+                        Example: '.NotIn(value0, value1, value2, value3, etc.)',
+                        Parameters: [
+                            {
+                                Name: 'value',
+                                Type: 'any',
+                                Infinite: true
+                            }
+                        ]
+                    }
+                ]
+            }
         ]
     },
     {
         Name: 'StartsWith',
         Targets: [
-             {
-                 TargetTypes: [
-                             window.pa.utils.DataTypes.String
-                 ],
-                 Overloads: [
-                     {
-                         Parameters: [
-                             {
-                                 Name: 'start',
-                                 Type: window.pa.utils.DataTypes.String,
-                                 Example: '.StartsWith(start)'
-                             }
-                         ]
-                     }
-                 ]
-             }
+            {
+                TargetTypes: [
+                    window.pa.utils.DataTypes.String
+                ],
+                Overloads: [
+                    {
+                        Example: '.StartsWith(start)',
+                        Parameters: [
+                            {
+                                Name: 'start',
+                                Type: window.pa.utils.DataTypes.String
+                            }
+                        ]
+                    }
+                ]
+            }
         ]
     },
     {
@@ -91,15 +120,16 @@ window.pa.auxiliaryFunctionsDescriptor = [
         Targets: [
             {
                 TargetTypes: [
-                            window.pa.utils.DataTypes.String
+                    window.pa.utils.DataTypes.String
                 ],
                 Overloads: [
                     {
+                        Example: '.EndsWith(start)',
                         Parameters: [
                             {
                                 Name: 'end',
-                                Type: window.pa.utils.DataTypes.String,
-                                Example: '.EndsWith(start)'
+                                Type: window.pa.utils.DataTypes.String
+                                
                             }
                         ]
                     }
